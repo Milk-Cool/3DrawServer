78f9c40cb44dcd995c421c2cf8108593b89eb64c
Milk-Cool <milan-pl@mail.ru>
2024-09-27 12:51:39 +0300

    faster screenshots

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from screeninfo import get_monitors
 from threading import Thread
 from signal import pthread_kill, SIGTSTP
 from math import ceil
+import zlib
+from time import sleep
 
 mouse = Controller()
 mouse_state = False
@@ -26,10 +28,15 @@ SCREEN_PACKET_SIZE = 512
 DEPTH = 3
 SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * DEPTH
 
+lock = False
+
 def send_thread(conn: socket.socket):
-    global x, y, SCREEN_WIDTH, SCREEN_HEIGHT, DEPTH
+    global lock, x, y, SCREEN_WIDTH, SCREEN_HEIGHT, DEPTH
     with mss.mss() as sct:
         while True:
+            while lock:
+                sleep(0.001)
+            print("lock released!")
             scale_fl = scale / 100
             left = x - int(scale_fl * (SCREEN_WIDTH // 2))
             top = y - int(scale_fl * (SCREEN_HEIGHT // 2))
@@ -44,9 +51,11 @@ def send_thread(conn: socket.socket):
             for tx in range(SCREEN_WIDTH):
                 for ty in range(SCREEN_HEIGHT):
                     out.extend(bytearray(pixels[int(height - ty * scale_fl - 1) * width + int(tx * scale_fl)][::-1]))
-            conn.send(bytes([(SIZE >> 16) & 0xff, (SIZE >> 8) & 0xff, (SIZE >> 0) & 0xff])
-                + bytes(out))
-            print("sent screen")
+            out_z = zlib.compress(bytes(out))
+            conn.send(bytes([(len(out_z) >> 16) & 0xff, (len(out_z) >> 8) & 0xff, (len(out_z) >> 0) & 0xff])
+                + out_z)
+            lock = True
+            print("sent screen and set lock!")
 
 print("Started up, please use ipconfig/ifconfig for IP")
 
@@ -93,6 +102,8 @@ while True:
             if ((x >= int((SCREEN_WIDTH // 2) * st) and x < dx - ceil((SCREEN_WIDTH // 2) * st) - 1)
                     and (y >= int((SCREEN_HEIGHT // 2) * st) and y < dy - ceil((SCREEN_HEIGHT // 2) * st) - 1)):
                 scale = min(300, scale + 1)
+        elif data_t == 0x06:
+            lock = False
         else:
             print("Invalid packet!!!")
     pthread_kill(t.ident, SIGTSTP)
